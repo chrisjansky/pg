c01021c6750845ec93f8d6c568e13dc110158deb
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-11-15 22:39:24 +0100

    JADE Wrapped people into .m-people

--- wp/wp-content/themes/pg/taxonomy.php
+++ wp/wp-content/themes/pg/taxonomy.php
@@ -1,39 +1,41 @@
 <?php get_header(); ?>
 
-  <?php
-  $args = array('hide_empty' => true);
+  <section class="m-people">
+    <?php
+    $args = array('hide_empty' => true);
 
-  $custom_taxonomies = array('student', 'graduate');
+    $custom_taxonomies = array('student', 'graduate');
 
-  foreach ($custom_taxonomies as $key=>$custom_taxonomy) {
-    $terms = get_terms($custom_taxonomy, $args);
-    if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
-      $count = count($terms);
-      $current_term = get_queried_object()->term_id;
-      $i = 0;
-      $term_list = '<ul class="o-namelist">';
-      foreach ($terms as $term) {
-        $i++;
-        if ($current_term == $term->term_id) {
-          $term_list .= '<li class="o-namelist__item"><span class="o-namelist__selected">' . $term->name . '</span></li>';
-        } else {
-          $term_list .= '<li class="o-namelist__item"><a class="o-namelist__link" href="' . get_term_link( $term ) . '" title="' . sprintf(__('View all post filed under %s', 'my_localization_domain'), $term->name) . '">' . $term->name . '</a></li>';
+    foreach ($custom_taxonomies as $key=>$custom_taxonomy) {
+      $terms = get_terms($custom_taxonomy, $args);
+      if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+        $count = count($terms);
+        $current_term = get_queried_object()->term_id;
+        $i = 0;
+        $term_list = '<ul class="o-namelist">';
+        foreach ($terms as $term) {
+          $i++;
+          if ($current_term == $term->term_id) {
+            $term_list .= '<li class="o-namelist__item"><span class="o-namelist__selected">' . $term->name . '</span></li>';
+          } else {
+            $term_list .= '<li class="o-namelist__item"><a class="o-namelist__link" href="' . get_term_link( $term ) . '" title="' . sprintf(__('View all post filed under %s', 'my_localization_domain'), $term->name) . '">' . $term->name . '</a></li>';
+          }
+          if ($count != $i) {
+            $term_list .= "";
+          }
+          else {
+            $term_list .= '</ul>';
+          }
         }
-        if ($count != $i) {
-          $term_list .= "";
+        echo '<h1 class="t-subhead">' . get_taxonomy($term->taxonomy)->label . '</h1>';
+        echo $term_list; 
+        if ($key == 0) {
+          echo '<hr class="l-divider--small">';
         }
-        else {
-          $term_list .= '</ul>';
-        }
-      }
-      echo '<h1 class="t-subhead">' . get_taxonomy($term->taxonomy)->label . '</h1>';
-      echo $term_list; 
-      if ($key == 0) {
-        echo '<hr class="l-divider--small">';
       }
     }
-  }
-  ?>
+    ?>
+  </section>
 
   <section class="m-single m-single--person">
     <div class="m-single__name">
